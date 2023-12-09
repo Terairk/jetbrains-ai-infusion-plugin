c551a462544aa383b21c6b0584947ad504c6cc3a
GitHub Action <action@github.com>
2023-12-09 14:59:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/terairk/jetbrainsaiinfusionplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/terairk/jetbrainsaiinfusionplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.terairk.jetbrainsaiinfusionplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.terairk.jetbrainsaiinfusionplugin.MyBundle
+import com.github.terairk.jetbrainsaiinfusionplugin.services.MyProjectService
 import javax.swing.JButton
 
 
